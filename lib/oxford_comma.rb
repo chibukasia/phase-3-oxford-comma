d3fa0f60f9b18eaccbcce6996dbd9e2030b3a90b
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-08-19 13:02:25 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    last_str = array[-1]
 
+    str = if array.length == 1
+            array.join()
+          elsif array.length == 2
+            array.join(' and ')
+          elsif array.length == 3
+            array.slice(0, 2).join(', ') << ", and #{last_str}"
+            #array
+          else
+            array.slice(0, array.length - 1).join(', ') << ", and #{last_str}"
+          end
 end
